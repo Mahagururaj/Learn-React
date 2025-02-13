246f243a732b21a1be52e3a6ec47880267cfcb57
Mahagururaj <107253409+Mahagururaj@users.noreply.github.com>
2025-02-13 14:47:03 +0530

    Add files via upload

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Hello from "./Hello"
+
+
+function App() {
+  const username = "Guru"
+
+  return (
+    <>
+    <Hello/>
+    <h1>{username}!</h1>
+    <p>Good Morning</p>
+   </>
+  )
+}
+
+export default App
